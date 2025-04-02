b283cd0429cbca181ac99db826577b5c87a69fa8
Matias Ponce <80530313+Juan-Matias@users.noreply.github.com>
2025-04-02 14:25:38 -0300

    Create 02_program_structure.js

---
+++ 02_program_structure.js
@@ -0,0 +1,17 @@
+'Codigo'
+Haciendo un triángulo con bucles
+Escribe un bucle que realice siete llamadas a console.log para mostrar el siguiente triángulo:
+
+#
+##
+###
+####
+#####
+######
+#######
+Puede ser útil saber que puedes encontrar la longitud de una cadena escribiendo .length después de ella.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+La mayoría de los ejercicios contienen un fragmento de código que puedes modificar para resolver el ejercicio. Recuerda que puedes hacer clic en los bloques de código para editarlos.
